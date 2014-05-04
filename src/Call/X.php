de59a4e4a3bd27e92d53bdf381d1382cc79e05a9
Luis Cordova <cordoval@gmail.com>
2014-05-03 19:22:39 -0500

    bootstrap key classes

---
+++ src/Call/X.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of frontend-block
+ *
+ * (c) Aaron Scherer <aequasi@gmail.com>
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE
+ */
+
+namespace Bldr\Block\Gush\Call;
+
+use Bldr\Call\AbstractCall;
+
+/**
+ * @author Luis Cordova <cordoval@gmail.com>
+ */
+class FlowCall extends AbstractCall
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function configure()
+    {
+        $this->setName('flow')
+            ->setDescription('This creates and takes an issue')
+        ;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function run()
+    {
+        
+    }
+}
---
+++ src/GushBlock.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of frontend-block
+ *
+ * (c) Aaron Scherer <aequasi@gmail.com>
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE
+ */
+
+namespace Bldr\Block\Gush;
+
+use Bldr\DependencyInjection\AbstractBlock;
+use Symfony\Component\DependencyInjection\ContainerBuilder as SymfonyContainerBuilder;
+
+/**
+ * @author Luis Cordova <cordoval@gmail.com>
+ */
+class GushBlock extends AbstractBlock
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function assemble(array $config, SymfonyContainerBuilder $container)
+    {
+        $this->addCall('bldr_frontend.less', 'Bldr\Block\Frontend\Call\LessCall');
+    }
+}
